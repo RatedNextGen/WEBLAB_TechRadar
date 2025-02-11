e663db82da902e15d1419011103beba92f80e3a6
taasane1 <nevilan.sabanathan@swisscom.com>
2025-02-11 10:19:39 +0100

    adds simple login page

--- tech-radar/apps/ui/src/app/app.config.ts
+++ tech-radar/apps/ui/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimationsAsync()
   ],
 };
--- tech-radar/apps/ui/src/app/pages/login/login.component.ts
+++ tech-radar/apps/ui/src/app/pages/login/login.component.ts
@@ -1,26 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButton],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
-  standalone: true
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginComponent {
-  email = '';
-  password = '';
+  loginForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router) {}
 
-  login(): void {
-    this.authService.login(this.email, this.password).subscribe({
-      next: () => this.router.navigate(['/']),
-      error: () => alert('Invalid credentials')
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(1)]]
     });
   }
+
+  login(): void {
+    if (this.loginForm.value) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: () => this.router.navigate(['/']),
+        error: () => alert('Invalid credentials')
+      });
+    }
+  }
+
+  get emailForm() {
+    return this.loginForm.controls['email'];
+  }
+
+  get passwordForm() {
+    return this.loginForm.controls['passwords'];
+  }
+
 }
